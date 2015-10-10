bee1e9e30c1cb7608f46ba58dc29e270d6524eaa
Aidan Smith <nuclearfarts99@gmail.com>
2015-10-10 13:00:20 -0400

    working on spawnitem

--- CodeMageMeteor/private/codemage/python/mpApi.py
+++ CodeMageMeteor/private/codemage/python/mpApi.py
@@ -25,3 +25,17 @@ def spawnentity(x, y, z, entity, data="{}"):
     entity = mc(player.getWorld().spawnEntity, loc(x, y, z), entity)
     cmd = "entitydata " + entity.getUniqueId().toString() + " " + data
     mc(player.getServer().dispatchCommand, player.getServer().getConsoleSender(), cmd)
+
+def spawnitem(x, y, z, item=DIRT, count=1, damage=0, data="{}"):
+    from org.bukkit.inventory import ItemStack
+    import json
+    dictdata = dict()
+    dictdata["Item"] = {"Count":count,"Damage":damage,"id":item.toString().lower()}
+    dictdata["Item"]["tag"] = json.loads(data)
+    strdata = json.dumps(dictdata)
+    #itemstack = ItemStack(item, count, damage)
+    #itementity = mc(player.getWorld().dropItem, loc(x, y, z), itemstack)
+    cmd = "summon Item " + str(x) + " " + str(y) + " " + str(z) + " " + strdata
+    print cmd
+    mc(player.getServer().dispatchCommand, player.getServer().getConsoleSender(), cmd)
+    #spawnentity(x, y, z, DROPPED_ITEM, strdata)

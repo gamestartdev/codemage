31b5dc05896a017bb6688743370cbcedc13f189c
Aidan Smith <nuclearfarts99@gmail.net>
2016-06-11 17:59:25 -0400

    updated Spigot to Minecraft 1.10

--- CodeMageSpigot/serverResources/launchServer.bat
+++ CodeMageSpigot/serverResources/launchServer.bat
@@ -1,3 +1,3 @@
 REM Update from Mercurial
-java -Xms512M -Xmx1024M -XX:MaxPermSize=128M -Dcom.mojang.eula.agree=true -jar spigot-1.8.8.jar --online-mode false
+java -Xms512M -Xmx1024M -XX:MaxPermSize=128M -Dcom.mojang.eula.agree=true -jar spigot-1.10.jar --online-mode false
 pause
